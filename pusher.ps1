05bb6d61955591afe0f06825163da731a8b9def0
Grant Steinfeld <grant.steinfeld.tech@gmail.com>
2022-05-19 23:17:10 -0400

    plumb

--- pusher.ps1
+++ pusher.ps1
@@ -1,6 +1,8 @@
 (65..90) + (97..122) | Get-Random -Count 5 | % {[char]$_}  | Set-Content -Encoding utf8 ./data.txt
 
-git pull
+
 git add ./data.txt
 git commit -m "off the local mac"
+
+git pull
 git push origin main    
