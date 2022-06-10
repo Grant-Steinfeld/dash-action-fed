bddff572c270c6fbd2ae6b8789ceaa08e001672c
Grant-Steinfeld <grant.steinfeld.tech@gmail.com>
2022-06-09 20:33:17 -0400

    new

--- build-dash-csv.ps1
+++ build-dash-csv.ps1
@@ -10,14 +10,18 @@ $d = [datetime]::ParseExact(
 
 $exactTime = Get-Date $d -UFormat %R
 $Header = 'humidity','temp','gas','timestamp','ts'
-$csvData = ConvertFrom-Csv -Header $Header ./data_cap/study_bme_1.csv -AsHashtable
 
+$path = "/home/pi/dev/dash-action-fed/data_cap/study_bme_1.csv"
+$csvData = Import-Csv -Header $Header -Path $path
 
+$table = $csvData | format-table
 
 
 $data = Get-Content -Raw ./template-BME.md
-$data = $data.replace("{psTime}", $psTime)
-$data = $data.replace("{todayDay}", $todayDay)
-$data = $data.replace("{exactTime}", $exactTime)
-$data = $data.replace("{csv}", $csvData)
-$data | Set-Content -Encoding utf8 ./BME-DASHBOARD.md
+#$data
+# $data = $data.replace("{psTime}", $psTime)
+# $data = $data.replace("{todayDay}", $todayDay)
+# $data = $data.replace("{exactTime}",
+ #$exactTime)
+ $data = $data.replace("{csv}", $table)
+ $data | Set-Content -Encoding utf8 ./BME-DASHBOARD.md
